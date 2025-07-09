9dcba21aab8212bb65299b665068f91fbad1de54
Prashant Sachan <your-email@example.com>
2025-07-09 23:28:12 +0530

    feat: update lucide-react to version 0.525.0 and add recharts dependency
    refactor: enhance Leave component with pie chart visualization using recharts
    
    chore: add TypeScript types for d3 libraries in yarn.lock
    
    fix: update redux toolkit and react-redux version constraints in yarn.lock
    
    style: improve chart styling and tooltip functionality in Chart component

--- src/page/me/leave/index.tsx
+++ src/page/me/leave/index.tsx
@@ -1,5 +1,52 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Pie, PieChart, Sector, Cell } from "recharts";
+import { PieSectorDataItem } from "recharts/types/polar/Pie";
+
+const chartData = [
+	{ browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
+	{ browser: "safari", visitors: 200, fill: "var(--color-safari)" },
+	{ browser: "firefox", visitors: 187, fill: "var(--color-firefox)" },
+	{ browser: "edge", visitors: 173, fill: "var(--color-edge)" },
+	{ browser: "other", visitors: 90, fill: "var(--color-other)" },
+];
+
 const Leave = () => {
-  return <div>Leave</div>;
+	return (
+		<div className="flex justify-center items-center min-h-screen">
+			<Card>
+				<CardHeader>
+					<CardTitle>Pie Chart - Donut Active</CardTitle>
+					<CardDescription>January - June 2024</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<PieChart width={300} height={200}>
+						<Pie
+							data={chartData}
+							dataKey="visitors"
+							nameKey="browser"
+							innerRadius={60}
+							outerRadius={80}
+							strokeWidth={5}
+              
+							activeShape={({
+								outerRadius = 0,
+								...props
+							}: PieSectorDataItem) => (
+								<Sector {...props} outerRadius={outerRadius + 10} />
+							)}
+						>
+							{chartData.map((entry, index) => (
+								<Cell
+									key={`cell-${index}`}
+									fill={`var(--color-${entry.browser})`}
+								/>
+							))}
+						</Pie>
+					</PieChart>
+				</CardContent>
+			</Card>
+		</div>
+	);
 };
 
 export default Leave;
