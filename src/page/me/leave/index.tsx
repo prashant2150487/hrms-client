a0804a3664f51127eeaec816b902d642d1d04f1d
Diksha <dikshatiwari9264@gmail.com>
2025-07-27 17:11:37 +0530

    feat: add Chart component to display leave statistics in the leave section

--- src/page/me/leave/index.tsx
+++ src/page/me/leave/index.tsx
@@ -12,13 +12,14 @@ import {
 } from "@/components/ui/drawer";
 import { X } from "lucide-react"; // Add this import
 import { useState } from "react";
+import Chart from "./chart";
 
 const Leave = () => {
   const [open, setOpen] = useState(false);
 
   return (
     <div className="p-3">
-      
+      <Chart/> 
         <Drawer open={open} onOpenChange={setOpen}>
           <DrawerTrigger asChild>
             <Button
@@ -46,7 +47,6 @@ const Leave = () => {
             </DrawerHeader>
           </DrawerContent>
         </Drawer>
-      
       <div className="grid grid-cols-12 gap-4 mt-3">
         <LeaveHistory />
         <LeaveCalender />
