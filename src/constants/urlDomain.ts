f81099b55663dd5f0afb9592e7dee5173c107306
Prashant Sachan <your-email@example.com>
2025-08-26 08:22:38 +0530

    feat: add urlDomain constants and update ManageLeaves component with proper formatting

---
+++ src/constants/urlDomain.ts
@@ -0,0 +1,38 @@
+const data = {
+  home: [
+    {
+      title: "Dashboard",
+      url: "/home/dashboard",
+    },
+    {
+      title: "Me",
+      url: "/home/me",
+    },
+  ],
+  me: [
+    {
+      title: "Attendence",
+      url: "/me/attendence",
+    },
+    {
+      title: "Leave",
+      url: "/me/leave",
+    },
+  ],
+  inbox: {},
+  myTeams: [],
+  admin: [
+    {
+      title: "Attendence",
+      url: "/me/attendence",
+    },
+    {
+      title: "Leave",
+      url: "/me/leave",
+    },
+    {
+      title: "Attendence",
+      url: "/me/attendence",
+    },
+  ],
+};
--- src/page/home/dashboard/admin/manageLeaves/index.tsx
+++ src/page/home/dashboard/admin/manageLeaves/index.tsx
@@ -1,11 +1,12 @@
-import SecondaryHeader from "@/components/SecondaryHeader"
+import SecondaryHeader from "@/components/SecondaryHeader";
 
 const ManageLeaves = () => {
   return (
     <div>
-      <SecondaryHeader data={["Onboard","Holiday","Manage Leaves"]} />
-      ManageLeaves</div>
-  )
-}
+      <SecondaryHeader data={["Onboard", "Holiday", "Manage Leaves"]} />
+      ManageLeaves
+    </div>
+  );
+};
 
-export default ManageLeaves
+export default ManageLeaves;
